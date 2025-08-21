2b9447071e6ba8b20a065589349cdcad4b3c91a7
rootp1 <arnav.iitr@gmail.com>
2025-08-21 14:50:28 +0530

    font import

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,10 @@ export default {
 			}
 		},
 		extend: {
+			fontFamily: {
+				sans: ['Inter', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+				mono: ['"IBM Plex Mono"', 'ui-monospace', 'SFMono-Regular', 'monospace']
+			},
 			colors: {
 				border: 'hsl(var(--border))',
 				input: 'hsl(var(--input))',
